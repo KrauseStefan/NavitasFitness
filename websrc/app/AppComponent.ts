69e44a78a391794e3c9fce36c15b0a271e9e7bde
Stefan Krause-Kjær <stefan.krausekjaer@gmail.com>
2015-12-25 23:00:16 +0100

    Added url routing

--- websrc/app/AppComponent.ts
+++ websrc/app/AppComponent.ts
@@ -1,17 +1,16 @@
 import {Component} from "angular2/core"
-// import {RouteConfig} from "angular2/router"
+import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router"
 import {MainPage} from "./MainPage/MainPage"
 
 @Component({
   selector: 'app-component',
   templateUrl: '/AppComponent.html',
-  directives: [MainPage]
+  directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  {path:'/',              name: 'FrontPage',    component: MainPage,  useAsDefault: true},
+  {path:'/front-page',    name: 'FrontPage',    component: MainPage},
+  {path:'/blog',          name: 'Blog',       component: MainPage},
+  {path:'/registration',  name: 'Registrer',  component: MainPage}
+])
 export class AppComponent { }
-// @RouteConfig([
-//   {path:'/main-page',     name: 'Forside',    component: MainPage},
-//   // {path:'/blog',          name: 'Blog',       component: HeroListComponent},
-//   // {path:'/registration',  name: 'Registrer',  component: HeroDetailComponent}
-// ])
-
-
--- websrc/app/boot.ts
+++ websrc/app/boot.ts
@@ -1,10 +1,12 @@
 import { bootstrap } from "angular2/platform/browser"
 import { HTTP_PROVIDERS } from "angular2/http"
+import { ROUTER_PROVIDERS } from "angular2/router"
 import { BlogPostsService } from "./MainPage/BlogPostsService"
 
 import {AppComponent} from "./AppComponent"
 
 bootstrap(AppComponent, [
     BlogPostsService,
-    HTTP_PROVIDERS
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS
   ])
