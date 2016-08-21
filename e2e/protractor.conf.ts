a0ab3e630df4033051ccbd83f4a49d66926ee8b0
KrauseStefan <stefan.krausekjaer@gmail.com>
2016-08-21 13:27:49 +0200

    Added basic e2e test

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -1,6 +1,15 @@
 
 
 
-export const config = {
+import {Config} from 'protractor';
 
-}
+export const config: Config = {
+  framework: 'jasmine',
+  baseUrl: 'http://localhost:9000',
+  capabilities: {
+    browserName: 'chrome'
+  // }, {
+  //   browserName: 'firefox'
+  },
+  specs: [ 'specs/*.js' ]
+};
---
+++ e2e/specs/spec1.spec.ts
@@ -0,0 +1,11 @@
+import {browser} from 'protractor/globals';
+// import {ProtractorBrowser} from 'protractor';
+// import {browser, by, By, element, $, $$, ExpectedConditions, protractor} from 'protractor/globals';
+
+describe('Navigation tests', () => {
+
+  it('should respond to the basic "/" address', () => {
+    browser.get('/')
+  });
+
+});
