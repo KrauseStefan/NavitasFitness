16a9133d11a5929ccc822ef039b0133e6ec629f0
krauseStefan <stefan.krausekjaer@gmail.com>
2016-11-07 23:42:58 +0100

    Better filter what is copied to libs

--- websrc/buildenv/copyLibs.js
+++ websrc/buildenv/copyLibs.js
@@ -3,8 +3,15 @@ const fse = require('fs-extra');
 const nodeModulesPath = './node_modules';
 const appLibPath = '../webapp';
 
+const filters = [
+    'angular-mocks',
+    'angular-sanitize',
+    'material-design-icons', // not used
+    'rxjs' // imported through typescript bundlin
+];
+
 Object.keys(require('../package.json').dependencies)
-    .filter(module => !(module.startsWith('@types') || module === 'material-design-icons'))
+    .filter(module => !module.startsWith('@types') && filters.indexOf(module) === -1)
     .forEach(module => {
         const source = fse.realpathSync(`${nodeModulesPath}/${module}`);
         const target = fse.realpathSync(`${appLibPath}`) + `/libs/${module}`;
