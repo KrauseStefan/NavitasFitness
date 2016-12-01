491fffa99d4032097ca425a959f25f382592c42c
krauseStefan <stefan.krausekjaer@gmail.com>
2016-12-01 22:46:19 +0100

    Added firefox information to protractor.conf.ts

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -6,29 +6,38 @@ declare const angular: any;
 declare const by: ProtractorBy;
 
 export const config: Config = {
-  baseUrl: 'http://localhost:8080',
-  // baseUrl: 'http://navitas-fitness-aarhus.appspot.com/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {defaultTimeoutInterval: timeoutMils},
-  multiCapabilities: [{
-    browserName: 'chrome',
-  // }, {
-  //   browserName: 'firefox'
-  }],
-  onPrepare: () => {
-    by.addLocator('linkUiSref', (toState: string, optParentElement: HTMLElement) => {
-      const using = optParentElement || document;
-      const tabs = using.querySelectorAll('md-tab-item');
+    baseUrl: 'http://localhost:8080',
+    // baseUrl: 'http://navitas-fitness-aarhus.appspot.com/',
+    // directConnect: true,
+    framework: 'jasmine',
+    jasmineNodeOpts: { defaultTimeoutInterval: timeoutMils },
+    multiCapabilities: [{
+        browserName: 'chrome',
+        maxInstances: 3,
+        // shardTestFiles: true,
+        // }, {
+        // browserName: 'firefox',
+        // marionette: true,
+        // // maxInstances: 3,
+        // // shardTestFiles: true,
+    }],
+    onPrepare: () => {
+        by.addLocator('linkUiSref', (toState: string, optParentElement: HTMLElement) => {
+            const using = optParentElement || document;
+            const tabs = using.querySelectorAll('md-tab-item');
 
-      for (let i = 0; tabs.length > i; i++) {
-        const uiRef = angular.element(tabs[i]).scope().tab.element.attr('ui-sref');
-        if (uiRef === toState) {
-          return tabs[i];
-        }
-      }
+            for (let i = 0; tabs.length > i; i++) {
+                const uiRef = angular.element(tabs[i]).scope().tab.element.attr('ui-sref');
+                if (uiRef === toState) {
+                    return tabs[i];
+                }
+            }
 
-      return null;
-    });
-  },
-  specs: ['specs/*.js'],
+            return null;
+        });
+    },
+    // seleniumArgs: [
+    // '-Dwebdriver.gecko.driver=./node_modules/protractor/node_modules/webdriver-manager/selenium/geckodriver-v0.11.1',
+    // ],
+    specs: ['specs/*.js'],
 };
