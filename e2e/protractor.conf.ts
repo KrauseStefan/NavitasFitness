e7d1c3c3dcf1af5f07ca575ee44327b4efbd6121
krauseStefan <stefan.krausekjaer@gmail.com>
2017-01-10 22:51:13 +0100

    Added automatic screenshot reporter

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -1,12 +1,44 @@
 import { Config, ProtractorBy } from 'protractor';
 
+import { PluginConfig } from 'protractor/built/plugins';
+
 const timeoutMils = 1000 * 60 * 10;
 
 declare const angular: any;
 declare const by: ProtractorBy;
 
+interface IJasmine2ProtractorUtilsConfig extends PluginConfig {
+    clearFoldersBeforeTest?: boolean;
+    disableHTMLReport?: boolean;
+    disableScreenshot?: boolean;
+    failTestOnErrorLog?: {
+        excludeKeywords: string[], // {A JSON Array}
+        failTestOnErrorLogLevel: number,
+    };
+    htmlReportDir?: string;
+    screenshotOnExpectFailure?: boolean;
+    screenshotOnSpecFailure?: boolean;
+    screenshotPath?: string;
+}
+
+const utilsPlugin: IJasmine2ProtractorUtilsConfig = {
+    clearFoldersBeforeTest: true,
+    disableHTMLReport: false,
+    disableScreenshot: false,
+    failTestOnErrorLog: {
+        excludeKeywords: [], // {A JSON Array}
+        failTestOnErrorLogLevel: 900,
+    },
+    htmlReportDir: './reports/htmlReports',
+    package: 'jasmine2-protractor-utils',
+    screenshotOnExpectFailure: true,
+    screenshotOnSpecFailure: true,
+    screenshotPath: './screenshots',
+};
+
 export const config: Config = {
     baseUrl: 'http://localhost:8080',
+    directConnect: true,
     // baseUrl: 'http://navitas-fitness-aarhus.appspot.com/',
     // directConnect: true,
     framework: 'jasmine',
@@ -36,6 +68,7 @@ export const config: Config = {
             return null;
         });
     },
+    plugins: [utilsPlugin],
     // seleniumArgs: [
     // '-Dwebdriver.gecko.driver=./node_modules/protractor/node_modules/webdriver-manager/selenium/geckodriver-v0.11.1',
     // ],
