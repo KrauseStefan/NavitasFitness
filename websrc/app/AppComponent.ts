61b24e250bd37fbac7a0074c3b365cde9d69cc3b
krauseStefan <stefan.krausekjaer@gmail.com>
2017-07-16 19:22:09 +0200

    Closes #23 Make selected tab update on initiation and login success

--- websrc/app/AppComponent.ts
+++ websrc/app/AppComponent.ts
@@ -6,13 +6,30 @@ import { element, isDefined, isObject } from 'angular';
 
 class AppComponentController {
 
+  public tabs = [{
+    id: 'MainPage',
+    icon: 'home',
+    text: 'Home',
+    disabled: () => false,
+  }, {
+    id: 'Status',
+    icon: 'perm_identity',
+    text: 'Payment Status',
+    disabled: () => !this.isLoggedIn(),
+  }];
+
+  public selectedTabIndex: number;
   private loggedInUser: IUserDTO;
 
   constructor(
     private $mdDialog: ng.material.IDialogService,
     private $mdMedia: ng.material.IMedia,
     private $location: ng.ILocationService,
+    private $scope: ng.IScope,
+    private $state: ng.ui.IStateService,
     private userService: UserService) {
+
+    this.updateSelectedTab();
 
     const searchParams = this.$location.search();
     if (isDefined(searchParams[KeyFieldName])) {
@@ -21,7 +38,18 @@ class AppComponentController {
 
     userService.getLoggedinUser$().subscribe((user: IUserDTO) => {
       this.loggedInUser = user;
+      this.updateSelectedTab();
     });
+  }
+
+  public updateSelectedTab() {
+    let index = this.tabs.findIndex(i => i.id === this.$state.current.name);
+    if (!this.tabs[index].disabled()) {
+      this.$scope.$applyAsync(() => {
+        // Selection cannot be changed while the tab is disabled, it takes a digest cycle before it is unlocked
+        this.selectedTabIndex = index;
+      });
+    }
   }
 
   public openPasswordChangeDialog() {
