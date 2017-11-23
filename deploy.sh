d2c6c8f7c2e9897df32e0f986877e67222eb4681
krauseStefan <stefan.krausekjaer@gmail.com>
2017-11-23 22:10:37 +0100

    Bumped app engine version

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH="$GOPATH:$root"
 
-gcloud app deploy $root/src/NavitasFitness/app.yaml -v 1
+gcloud app deploy $root/src/NavitasFitness/app.yaml -v 1-1
