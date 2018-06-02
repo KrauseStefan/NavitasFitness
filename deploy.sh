1f4b2ff9fcb76aee7cf4d35df75d968012825f1d
krauseStefan <stefan.krausekjaer@gmail.com>
2018-06-02 17:36:07 +0200

    Change project target to admin

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH="$GOPATH:$root"
 
-gcloud app deploy $root/src/NavitasFitness/app.yaml -v 1-2
+gcloud app deploy $root/src/NavitasFitness/app.yaml -v admin --no-promote --no-stop-previous-version
