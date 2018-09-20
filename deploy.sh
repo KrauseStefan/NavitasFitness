40e379dab897aed3ee676a8b3504e79f5edcf0be
krauseStefan <stefan.krausekjaer@gmail.com>
2018-09-20 22:48:28 +0200

    Deploying 1.3

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH="$GOPATH:$root"
 
-gcloud app deploy $root/src/NavitasFitness/app.yaml -v beta --no-promote --no-stop-previous-version
+# gcloud app deploy $root/src/NavitasFitness/app.yaml -v beta --no-promote --no-stop-previous-version
+gcloud app deploy $root/src/NavitasFitness/app.yaml -v 1-3
