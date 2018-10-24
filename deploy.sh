bc3d387d8aecad8ec29cd98926528ebd61b8878b
krauseStefan <stefan.krausekjaer@gmail.com>
2018-10-24 19:40:39 +0200

    Bump Version number

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,8 @@
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH="$GOPATH:$root"
 
-version=v1-5-0
+version=v1-6-0
 
-# gcloud app deploy $root/src/NavitasFitness/app.yaml -v beta --no-promote --no-stop-previous-version
-# gcloud app deploy $root/src/NavitasFitness/app.yaml -v $version
 gcloud app deploy $root/src/NavitasFitness/app.yaml -v $version --no-promote --no-stop-previous-version
 git tag -f $version
 git push origin --tags
