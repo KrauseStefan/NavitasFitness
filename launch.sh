d4fe2a4b3ba65b39bffe8b713f149155b30ea515
krauseStefan <stefan.krausekjaer@gmail.com>
2019-01-19 14:28:11 +0100

    Added commented out start command for dev_appserver with full debug logging

--- launch.sh
+++ launch.sh
@@ -16,3 +16,4 @@ cd -
 
 #gnome-terminal -e "dev_appserver.py $root/src/NavitasFitness/app.yaml" &
 gnome-terminal --tab -e "npm start" --working-directory="$root/websrc" --title="client" --tab -e "npm start" --working-directory="$root/ipn-simulator" --title="IPN" --tab -e "dev_appserver.py --dev_appserver_log_level=warning $root/src/NavitasFitness/app.yaml" --title="server"
+# gnome-terminal --tab -e "npm start" --working-directory="$root/websrc" --title="client" --tab -e "npm start" --working-directory="$root/ipn-simulator" --title="IPN" --tab -e "dev_appserver.py $root/src/NavitasFitness/app.yaml" --title="server"
