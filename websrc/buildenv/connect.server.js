49779ccc4aa569bc65ce0e01de856aadac38e4a2
skk <stefan.krausekjaer@gmail.com>
2020-12-01 21:16:08 +0100

    Removed redundant code from connect.server.js

--- websrc/buildenv/connect.server.js
+++ websrc/buildenv/connect.server.js
@@ -3,19 +3,12 @@
 const connect = require('connect');
 const modRewrite = require('connect-modrewrite');
 const serveStatic = require('serve-static');
-const fs = require('fs-extra');
 
 const serverPort = 9000;
-const libPath = '/libs/';
 
 // below paths are relative to this script
 const appServerFolder = __dirname + '/../../src/NavitasFitness/webapp';
 const srcServeFolder = __dirname + '/../app';
-const outputLibs = appServerFolder + libPath;
-
-const libaryFolders = [
-  '/node_modules'
-].map((lib) => __dirname + '/..' + lib);
 
 connect()
   .use('/src', serveStatic(srcServeFolder))
