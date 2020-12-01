ddc777b8040c7feb3d0fd044390690cdaf594f72
skk <stefan.krausekjaer@gmail.com>
2020-12-01 21:13:20 +0100

    Added more information to init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-#Install golang
+# Install golang
 sudo apt install golang-1.12
 
-$GOHOME="$HOME/gopath"
-echo '' >> ~/.profile
-echo '# Golang Settings' >> ~/.profile
-echo 'PATH="/usr/lib/go-1.12/bin:$PATH"' >> ~/.profile
-echo 'GOPATH="$GOHOME"' >> ~/.profile
+GOHOME="$HOME/gopath"
+cat >> ~/.profile <<- EOM
+# Golang Settings
+PATH="/usr/lib/go-1.12/bin:$PATH"
+GOPATH="$GOHOME"
+EOM
+
 mkdir $GOHOME
 
-#download dependencies
+
+# download dependencies
 go get -v ./...
 go get gopkg.in/validator.v2
 
@@ -24,3 +27,7 @@ sudo apt install google-cloud-sdk google-cloud-sdk-app-engine-python google-clou
 gcloud init
 
 
+# Install chrome on ubuntu (or wsl2)
+curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
